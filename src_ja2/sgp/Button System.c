87f51cf09f0b41b2c58e92c9c8f38d7a7130d979
Manuel K <Manuel-K@users.noreply.github.com>
2018-01-13 18:33:29 +0100

    PATCH003: add BUTTON_ENABLED check to QuickButtonCallbackMButn() handling of BUTTON_CHECKBOX

--- src_ja2/sgp/Button System.c
+++ src_ja2/sgp/Button System.c
@@ -2699,7 +2699,7 @@ void QuickButtonCallbackMButn( MOUSE_REGION *reg, INT32 reason )
 			b->uiFlags &= (~BUTTON_CLICKED_ON);
 		}
 	}
-	else if( b->uiFlags & BUTTON_CHECKBOX )
+	else if( b->uiFlags & BUTTON_CHECKBOX && b->uiFlags & BUTTON_ENABLED )
 	{
 		if( reason & MSYS_CALLBACK_REASON_LBUTTON_DWN )
 		{	//the check box button gets anchored, though it doesn't actually use the anchoring move callback.
