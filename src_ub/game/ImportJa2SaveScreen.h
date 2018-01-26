aff10c8420d32d1c35a243f3f0ea3b5687beefa3
Manuel K <Manuel-K@users.noreply.github.com>
2018-01-26 18:30:22 +0100

    Use script to move shared files to src_common

--- src_ub/game/ImportJa2SaveScreen.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IMPORTJA2SAVE_SCREEN__H_
-#define _IMPORTJA2SAVE_SCREEN__H_
-
-extern UINT32 ImportJa2SaveScreenInit(void);
-extern UINT32 ImportJa2SaveScreenHandle(void);
-extern UINT32 ImportJa2SaveScreenShutdown(void);
-
-#endif
