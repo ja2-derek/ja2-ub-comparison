beadbc842b996093cf5c984889ce28f61ceadda2
Manuel K <Manuel-K@users.noreply.github.com>
2018-01-26 17:49:07 +0100

    PATCH035: use ClearMainMenu() for all languages in InitJA2SplashScreen()

--- src_ja2/game/JA2 Splash.c
+++ src_ja2/game/JA2 Splash.c
@@ -14,9 +14,6 @@ void InitJA2SplashScreen()
 	UINT32 uiLogoID = 0;
 	STRING512			CurrentDir;
 	STRING512			DataDir;
-	HVSURFACE hVSurface;
-	VSURFACE_DESC VSurfaceDesc;
-	INT32 i = 0;
 
 	InitializeJA2Clock();
 	//InitializeJA2TimerID();
@@ -34,26 +31,7 @@ void InitJA2SplashScreen()
 	//Initialize the file database
 	InitializeFileDatabase( gGameLibaries, NUMBER_OF_LIBRARIES );
 
-	
-	#ifdef ENGLISH
-		ClearMainMenu();
-	#else
-		{
-
-			memset( &VSurfaceDesc, 0, sizeof( VSURFACE_DESC ) );
-			VSurfaceDesc.fCreateFlags = VSURFACE_CREATE_FROMFILE | VSURFACE_SYSTEM_MEM_USAGE;
-			GetMLGFilename( VSurfaceDesc.ImageFile, MLG_SPLASH );
-			if( !AddVideoSurface( &VSurfaceDesc, &uiLogoID ) )
-			{	
-				AssertMsg( 0, String( "Failed to load %s", VSurfaceDesc.ImageFile ) );
-				return;
-			}
-
-			GetVideoSurface( &hVSurface, uiLogoID );
-			BltVideoSurfaceToVideoSurface( ghFrameBuffer, hVSurface, 0, 0, 0, 0, NULL );
-			DeleteVideoSurfaceFromIndex( uiLogoID );
-		}
-	#endif
+	ClearMainMenu();
 
 
 	InvalidateScreen();
